f0405df9f09f80e2f8568da16b75a315821df9f1
Anshul077 <anshulkamal07@gmail.com>
2022-11-30 16:20:35 +0530

    Added Changes

--- server/Connection.js
+++ server/Connection.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose"
 
-export const Connection=async()=>{
+export const Connection=async(URL)=>{
     try{
 
-        await mongoose.connect("mongodb+srv://user:12345@foodappcluster.pmo750z.mongodb.net/?retryWrites=true&w=majority",{ useUnifiedTopology: true, useNewUrlParser: true })
+        await mongoose.connect(URL,{ useUnifiedTopology: true, useNewUrlParser: true })
         console.log("Database connected!!")
     }
     catch{
--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from "express"
 import { Connection } from "./Connection.js"
 import { ExportData } from "./constants/ExportData.js"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app=express()
 
 const PORT=8000
 
+const URL=process.env.URL
+// const username = process.env.DB_USERNAME;
+// const password = process.env.DB_PASSWORD;
+
 app.listen(PORT,()=>{
     console.log(`Serve running on PORT ${PORT}`)
 })
 
-Connection()
+Connection(URL)
 ExportData()
