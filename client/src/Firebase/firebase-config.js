ded5a69ff6a7f64d938e7528814ae02868adf70b
Anshul077 <anshulkamal07@gmail.com>
2022-12-02 17:16:04 +0530

    Added Firebase

---
+++ client/src/Firebase/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAv050ve8jUUwzw8aVdTYU1MncO4ULkFF8",
+    authDomain: "food-app-95e27.firebaseapp.com",
+    projectId: "food-app-95e27",
+    storageBucket: "food-app-95e27.appspot.com",
+    messagingSenderId: "868202758173",
+    appId: "1:868202758173:web:5e79e05b95434307a486f4"
+  };
+
+  
+  export default firebaseConfig
--- server/Controller/Item.js
+++ server/Controller/Item.js
@@ -2,10 +2,7 @@ import data from "../model/DataSchema.js";
 
 export const getDataItem = async (request, response) => {
     try {
-        console.log("hello")
-        // console.log("category: ",request.body.category)
         const products = await data.find({});
-        console.log(products)
         response.json(products);
         
     } catch (error) {
