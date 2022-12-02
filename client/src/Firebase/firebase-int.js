3f096100f949bfcbbc8507a575c2cd430c1a2d7a
Anshul077 <anshulkamal07@gmail.com>
2022-12-02 17:30:34 +0530

    Added Changes

--- client/src/Firebase/firebase-int.js
+++ client/src/Firebase/firebase-int.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import firebaseConfig from "./Firebase-config";
+import firebaseConfig from "./firebase-config";
 
 const initializeAuthentication=()=>{
     initializeApp(firebaseConfig);
