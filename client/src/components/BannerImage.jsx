4461bfa1016951f459ee137e92cd8ee527617d71
Anshul077 <anshulkamal07@gmail.com>
2022-12-10 19:43:31 +0530

    added changes

--- client/src/components/BannerImage.jsx
+++ client/src/components/BannerImage.jsx
@@ -18,8 +18,10 @@ z-index:2;
 position:relative;
 top:10vh;
 left:-10vw;
+background-color: #ffffff73;
+border-radius:13px;
 box-shadow: inset 0 0 0 3000px rgba(150, 150, 150, 0.192);
-filter: blur(10px);
+backdrop-filter: blur(10px);
 `
 const MainContainBox=styled(Box)`
 height:100%;
@@ -27,7 +29,7 @@ width:fit-content;
 display:grid;
 float:right;
 grid-template-columns:200px 200px;
-grid-gap:100px 20px;
+grid-gap:100px 10px;
 `
 
 
@@ -36,16 +38,16 @@ const BannerImage = () => {
     <MainContainBox>
         <img src={heroBg} style={{objectFit:"contain",height:"100%",width:"31vw",position:"absolute",zIndex:1}}/>
     <ContainBox>
-    <img style={{height:"160px",position:"relative",top:"-80px"}} src="https://lh6.googleusercontent.com/F9wUCBnxawMk0D3uHEYOeh3QnRxMydAKKSVci2prethn0ZI3tQkaXAlgXqD3omOrdCY=w2400" alt="images"/>
+    <img style={{height:"160px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh6.googleusercontent.com/F9wUCBnxawMk0D3uHEYOeh3QnRxMydAKKSVci2prethn0ZI3tQkaXAlgXqD3omOrdCY=w2400" alt="images"/>
     </ContainBox>
     <ContainBox>
-    <img style={{height:"160px",position:"relative",top:"-80px"}} src="https://lh6.googleusercontent.com/F9wUCBnxawMk0D3uHEYOeh3QnRxMydAKKSVci2prethn0ZI3tQkaXAlgXqD3omOrdCY=w2400" alt="images"/>
+    <img style={{height:"160px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh6.googleusercontent.com/gHQJUopNhbsg-pXVA7lFWgEuRKSyy-5ELqYbpmmmgwcxpPHZx-NSu50nq1JNnE5_AAY=w2400" alt="images"/>
     </ContainBox>
     <ContainBox>
-    <img style={{height:"160px",position:"relative",top:"-80px"}} src="https://lh6.googleusercontent.com/F9wUCBnxawMk0D3uHEYOeh3QnRxMydAKKSVci2prethn0ZI3tQkaXAlgXqD3omOrdCY=w2400" alt="images"/>
+    <img style={{height:"160px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh4.googleusercontent.com/PgeUp3b8VA2G57EfQbonblzuduYcF6mlBrBYoWM-F9MmN50Dvo1NjYsEOVwEVwdoaZM=w2400" alt="images"/>
     </ContainBox>
     <ContainBox>
-    <img style={{height:"160px",position:"relative",top:"-80px"}} src="https://lh6.googleusercontent.com/F9wUCBnxawMk0D3uHEYOeh3QnRxMydAKKSVci2prethn0ZI3tQkaXAlgXqD3omOrdCY=w2400" alt="images"/>
+    <img style={{height:"125px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh5.googleusercontent.com/dOo0d_Mzd59HLFvt_sm9cKmYoCq215h5uNbAHDRwPByjieBg51mNwFMWvDoeyrr86a4=w2400" alt="images"/>
     </ContainBox>
     </MainContainBox>
   )
