0a8ff9d8bbb114e4c5e70bfcb961f1a40a69c2c3
Anshul077 <anshulkamal07@gmail.com>
2022-12-10 20:03:12 +0530

    added changes

--- client/src/components/BannerImage.jsx
+++ client/src/components/BannerImage.jsx
@@ -11,17 +11,21 @@ import {
 import heroBg from '../images/heroBg.png'
 
 const ContainBox=styled(Box)`
-height:100%;
+height:inherit;
 width:fit-content;
 padding:10px;
 z-index:2;
 position:relative;
 top:10vh;
-left:-10vw;
+left:-5vw;
 background-color: #ffffff73;
 border-radius:13px;
 box-shadow: inset 0 0 0 3000px rgba(150, 150, 150, 0.192);
 backdrop-filter: blur(10px);
+// display:flex;
+// flex-direction:column;
+// align-items:center;
+// justify-content:center;
 `
 const MainContainBox=styled(Box)`
 height:90%;
@@ -30,7 +34,16 @@ width:fit-content;
 display:grid;
 float:right;
 grid-template-columns:200px 200px;
-grid-gap:100px 10px;
+grid-gap:100px 5px;
+`
+const DetailsItem=styled(Box)`
+height:auto;
+display:flex;
+align-items:center;
+flex-direction:column;
+position:relative;
+width:fit-content;
+top:-55px;
 `
 
 
@@ -40,15 +53,35 @@ const BannerImage = () => {
         <img src={heroBg} style={{objectFit:"contain",height:"90%",width:"31vw",position:"absolute",zIndex:1}}/>
     <ContainBox>
     <img style={{height:"130px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh6.googleusercontent.com/F9wUCBnxawMk0D3uHEYOeh3QnRxMydAKKSVci2prethn0ZI3tQkaXAlgXqD3omOrdCY=w2400" alt="images"/>
+    <DetailsItem>
+    <Typography>Icecream</Typography>
+    <Typography>Chocolate & Vanilla</Typography>
+    <Typography>$ 5.25</Typography>
+    </DetailsItem>
     </ContainBox>
     <ContainBox>
     <img style={{height:"130px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh6.googleusercontent.com/gHQJUopNhbsg-pXVA7lFWgEuRKSyy-5ELqYbpmmmgwcxpPHZx-NSu50nq1JNnE5_AAY=w2400" alt="images"/>
+    <DetailsItem>
+    <Typography>Icecream</Typography>
+    <Typography>Chocolate & Vanilla</Typography>
+    <Typography>$ 5.25</Typography>
+    </DetailsItem>
     </ContainBox>
     <ContainBox>
     <img style={{height:"130px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh4.googleusercontent.com/PgeUp3b8VA2G57EfQbonblzuduYcF6mlBrBYoWM-F9MmN50Dvo1NjYsEOVwEVwdoaZM=w2400" alt="images"/>
+    <DetailsItem>
+    <Typography>Icecream</Typography>
+    <Typography>Chocolate & Vanilla</Typography>
+    <Typography>$ 5.25</Typography>
+    </DetailsItem>
     </ContainBox>
     <ContainBox>
-    <img style={{height:"125px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh5.googleusercontent.com/dOo0d_Mzd59HLFvt_sm9cKmYoCq215h5uNbAHDRwPByjieBg51mNwFMWvDoeyrr86a4=w2400" alt="images"/>
+    <img style={{height:"105px",position:"relative",top:"-80px",zIndex:"3"}} src="https://lh5.googleusercontent.com/dOo0d_Mzd59HLFvt_sm9cKmYoCq215h5uNbAHDRwPByjieBg51mNwFMWvDoeyrr86a4=w2400" alt="images"/>
+    <DetailsItem>
+    <Typography>Icecream</Typography>
+    <Typography>Chocolate & Vanilla</Typography>
+    <Typography>$ 5.25</Typography>
+    </DetailsItem>
     </ContainBox>
     </MainContainBox>
   )
